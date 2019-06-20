a6edded00369bbaeb16d2e067e5cf5d7db97f754
reiwa <reiwa@outlook.com.vn>
2019-06-21 02:34:14 +0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/styles'
+import React from 'react'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const classes = useStyles()
+
+  return <div className={classes.root}></div>
 }
 
-export default App;
+const useStyles = makeStyles(theme => {
+  return {
+    root: {}
+  }
+})
+
+export default App
